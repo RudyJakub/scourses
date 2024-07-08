adee12e2e1bd310c22d01f404172067ef5026e01
RudyJakub <kubazewsi@gmail.com>
2024-07-08 15:50:38 +0200

    staticfiles and templates

--- app/index.ts
+++ app/index.ts
@@ -1,22 +1,27 @@
-import express, { Request, Response , Application } from 'express';
-import db from './database';
+import express, { Request, Response , Application } from 'express'
+import db from './database'
+import nunjucks from "nunjucks"
 
 const app: Application = express();
 const port = 3000;
 
+app.use("/static", express.static(__dirname + "/static"))
+app.use(express.json())
+app.engine('html', nunjucks.render)
+app.set('view engine', 'html')
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+})
+
 app.get('/', async (req: Request, res: Response) => {
-  console.log(await db.query('SELECT NOW()'))
-  res.send('Welcome to Express & TypeScript Server');
-});
+    res.status(200).render("home")
+})
 
 db
 .connect()
 .then(() => {
     console.log("Connected to PostgreSQL")
-    app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`)
-    })
+    app.listen(port, () => { console.log(`Server is running at http://localhost:${port}`) })
 })
-.catch((err) => {
-    console.error(err)
-})
+.catch((err) => { console.error(err) })
